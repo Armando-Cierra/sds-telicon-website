ff9e9ebdb45f16fea389fcc44cd8fe48df08086f
Armando-Cierra <armando_cierra@outlook.com>
2021-08-27 19:26:31 -0600

    Theme feature improved

--- src/utils/syncTheme.ts
+++ src/utils/syncTheme.ts
@@ -1,4 +1,5 @@
 const syncTheme = () => {
+  //Detetcts automatically which is the user's operative system preferences
   const setThemeAutomatically = () => {
     const darkTheme = window.matchMedia('(prefers-color-scheme: dark)');
     if (darkTheme.matches) {
@@ -8,16 +9,23 @@ const syncTheme = () => {
     }
   };
 
-  window
-    .matchMedia('(prefers-color-scheme: dark)')
-    .addEventListener('change', setThemeAutomatically);
+  const savedTheme = window.localStorage.getItem('theme');
+  const systemPreference = window.matchMedia('(prefers-color-scheme: light)');
 
-  const savedTheme = window.localStorage.getItem('themePreferences');
-
+  //Verifies if there is a user preference already saved
   if (!savedTheme) {
     setThemeAutomatically();
+    localStorage.setItem('theme', 'Auto');
+    //Automatically changes the theme based on the system actions
+    systemPreference.addEventListener('change', setThemeAutomatically);
   } else {
-    document.body.setAttribute('data-theme', savedTheme);
+    if (savedTheme === 'Auto') {
+      setThemeAutomatically();
+      //Automatically changes the theme based on the system actions
+      systemPreference.addEventListener('change', setThemeAutomatically);
+    } else {
+      document.body.setAttribute('data-theme', savedTheme);
+    }
   }
 };
 
